2109ed0082eae0ed3cc474cac2f0dea667a96021
Geoff Hutchison <geoff.hutchison@gmail.com>
2026-03-08 14:09:41 -0400

    Fix some warnings
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

--- src/avogadro_genice/generator.py
+++ src/avogadro_genice/generator.py
@@ -2,6 +2,9 @@
 #  This source code is released under the 3-Clause BSD License, (see "LICENSE").
 #  https://github.com/ghutchis/avogadro-genice/
 
+import warnings
+
+import numpy as np
 from genice2.genice import GenIce
 from genice2.plugin import Lattice, Format, Molecule
 
@@ -13,11 +16,14 @@ def generate(opts):
     formatter = Format('cif')  # adds periodic vectors
     water = Molecule(options['model'])
 
+    np.random.seed(options['seed'])
+
     a = int(options['a'])
     b = int(options['b'])
     c = int(options['c'])
-    s = int(options['seed'])
-    ice = GenIce(lattice, rep=(a, b, c), seed=s).generate_ice(formatter, water=water)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        ice = GenIce(lattice, rep=(a, b, c)).generate_ice(formatter, water=water)
 
     return ice
 
